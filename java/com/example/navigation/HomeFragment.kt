6945bfc1bc0d88ff1953f88102643e3c6e03eac8
Himanshu Garg <hgarg701@gmail.com>
2020-06-08 01:17:08 -0700

    Added more fragments

--- java/com/example/navigation/HomeFragment.kt
+++ java/com/example/navigation/HomeFragment.kt
@@ -1,42 +1,41 @@
 package com.example.navigation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentController
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.example.navigation.databinding.FragmentHomeBinding
 
 /**
  * A simple [Fragment] subclass.
  */
-class HomeFragment : Fragment(), View.OnClickListener {
+class HomeFragment : Fragment() {
 
-    private lateinit var navController: NavController
+    private lateinit var binding : FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        navController = Navigation.findNavController(view)
-        view.findViewById<Button>(R.id.next).setOnClickListener(this)
-        view.findViewById<Button>(R.id.previous).setOnClickListener(this)
-    }
-
-    override fun onClick(v: View?) {
-        when(v!!.id){
-            R.id.next -> navController.navigate(R.id.action_homeFragment_to_nextFragment)
-            R.id.previous -> navController.navigate(R.id.action_homeFragment_to_previousFragment)
+        binding.next.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_nextFragment)
         }
+
+        binding.previous.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_previousFragment)
+        }
+        return binding.root
     }
 
 }
--- java/com/example/navigation/NextFragment.kt
+++ java/com/example/navigation/NextFragment.kt
@@ -5,18 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.navigation.databinding.FragmentNextBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class NextFragment : Fragment() {
 
+    private lateinit var binding : FragmentNextBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_next, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_next, container, false)
+
+        binding.nextDistant.setOnClickListener {
+            it.findNavController().navigate(R.id.action_nextFragment_to_nextDistant2)
+        }
+        return binding.root
     }
 
 }
